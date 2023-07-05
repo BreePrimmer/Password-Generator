1303e91ebf7d8b7b9c9dbe7a489b45e5c3b547d8
breelovesfrogs <134086243+breelovesfrogs@users.noreply.github.com>
2023-07-05 12:55:55 -0500

    working on generatePassword function

--- Develop/script.js
+++ Develop/script.js
@@ -9,9 +9,40 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
+  // This code is saying that the passwordText is equal to password,
+  // So when we press the generate button, and generate a password, the password
+  // Text area will display the password.
   passwordText.value = password;
 
+  function generatePassword () {
+    var length = window.prompt("Please choose a number between 8 and 128 \nfor your password length.")
+    console.log(length)
+    var lengthNum = parseInt(length)
+    if (lengthNum < 8 || lengthNum >128) {
+      var invalidNum = parseInt(window.prompt("Please enter a number between 8 and 128."))
+      console.log(invalidNum)
+    } else if (lengthNum >= 8 && lengthNum <=128) {
+      var lowercase = window.prompt("A good password should have different character types. \nLet's choose some character types for your password! \nWould you like to include lowercase characters?")
+    } else if (lowercase == "yes" || lowercase == "no") {
+      var uppercase = window.prompt("Would you like to include uppercase characters?")
+    } else if (uppercase == "yes" || uppercase == "no") {
+      var numeric = window.prompt("Would you like to include numeric characters?")
+    } else if (numeric == "yes" || numeric == "no") {
+      window.prompt("Would you like to include special characters?")
+    }
+  }
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// generatePassword () {
+//   var length = window.prompt("Please choose a number between 8 and 128 \nfor your password length.")
+//   var lengthNum = length.parseInt()
+//   if (lengthNum < 8 || lengthNum >128) {
+//     window.prompt("Please enter a number between 8 and 128.")
+//   } else if (lengthNum >= 8 && lengthNum <=128) {
+//     window.prompt("A good password should have different character types. \nLet's choose some character types for your password! \nWould you like to include lowercase characters?")
+// }}
+
+// generatePassword()
